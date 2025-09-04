54ce68a7376d859d9a43d4e4ad5e652dabd8efa2
EduxDeveloper <20230690@ricaldone.edu.sv>
2025-09-04 15:44:44 -0600

    commit coockie

---
+++ src/main/java/IntegracionBackFront/backfront/Services/Auth/AuthServices.java
@@ -0,0 +1,39 @@
+package IntegracionBackFront.backfront.Services.Auth;
+
+import IntegracionBackFront.backfront.Config.Argon2.Argon2Password;
+import IntegracionBackFront.backfront.Entities.Users.UserEntity;
+import IntegracionBackFront.backfront.Repositories.Users.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthServices {
+
+    @Autowired
+    private UserRepository repo;
+
+
+    public boolean Login(String correo, String contrasena){
+        Argon2Password objHash = new Argon2Password();
+        Optional<UserEntity> list = repo.findByCorreo(correo).stream().findFirst();
+        if (list.isPresent()){
+            UserEntity usuario = list.get();
+            String nombreTipoUsuario = usuario.getTipoUsuario().getNombreTipo();
+            System.out.println("Usuario ID encontrado " + usuario.getId()+
+                    ", email: " + usuario.getCorreo() +
+                    ", rol: " + nombreTipoUsuario);
+
+            return objHash.VerefyPassword(usuario.getContrasena(),contrasena);
+        }
+        return false;
+    }
+
+    public Optional<UserEntity> obtenerUsuario(String email){
+        Optional<UserEntity> userOPT = repo.findByCorreo(email);
+        return (userOPT != null) ? userOPT : null;
+    }
+
+
+}
